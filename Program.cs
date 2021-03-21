fd320f7f7c8741aaf6d0dc87c94f89227ab3d28e
Zoryana Sobol <Zoryana.Sobol@jumar-technology.com>
2021-03-21 20:53:35 +0200

    First commit for lab 1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using MihaZupan;
+
+namespace RachelBot
+{
+    class Program
+    {
+        private static ITelegramBotClient botClient;
+        static void Main(string[] args)
+        {
+            botClient = new TelegramBotClient("1373867194:AAHbsrXtDzusn-8AWywzAELTr_2hLMNtGms") {Timeout = TimeSpan.FromSeconds(10)};
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine($"Bot Id: {me.Id}, Bot name: {me.FirstName}");
+        }
+    }
+}
